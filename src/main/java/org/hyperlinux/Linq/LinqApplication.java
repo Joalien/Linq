00bd83f21f6ab8d46b2c9465f39f6566642a6f5f
Josquin Cornec <josquin@victoria.frmug.org>
2018-03-26 20:33:50 +0200

    Version de production qui fonctionne avec mvn spring-boot:run

--- src/main/java/org/hyperlinux/Linq/LinqApplication.java
+++ src/main/java/org/hyperlinux/Linq/LinqApplication.java
@@ -2,10 +2,19 @@ package org.hyperlinux.Linq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class LinqApplication {
+@SpringBootApplication()
+public class LinqApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(LinqApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LinqApplication.class);
+	}
 }
+
+
