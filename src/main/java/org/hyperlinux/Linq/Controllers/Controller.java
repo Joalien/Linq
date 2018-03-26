438379f055e685c25c600a74a21aff7f076f032a
Josquin Cornec <josquin@victoria.frmug.org>
2018-03-26 17:12:16 +0200

    Ajout d'un artefact

---
+++ src/main/java/org/hyperlinux/Linq/Controllers/Controller.java
@@ -0,0 +1,20 @@
+package org.hyperlinux.Linq.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+    @RequestMapping("/")
+    public ResponseEntity<?> createOrder() {
+        try {
+            return new ResponseEntity<>("Success\n", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error\n", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
