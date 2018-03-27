b88bdc3597459a14af66d03f4301179d1462d928
Josquin Cornec <josquin@victoria.frmug.org>
2018-03-28 00:34:12 +0200

    Mise en place d'un serveur REST API

---
+++ src/main/java/org/hyperlinux/Linq/Controllers/UserController.java
@@ -0,0 +1,65 @@
+package org.hyperlinux.Linq.Controllers;
+
+import org.hyperlinux.Linq.Model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+public class UserController {
+
+    private static Set<User> users = new HashSet<User>();
+
+    //Get all users to the list
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllUsers() {
+        try {
+            return new ResponseEntity<>(users.toString(), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //Post a user to the list
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<?> addUser(@RequestBody User user) {
+        try {
+            users.add(user);
+            return new ResponseEntity<>(users.toString(), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //Update an user
+    @RequestMapping(value = "/{pseudo}", method = RequestMethod.PUT)
+    public ResponseEntity<?> addUser(@PathVariable("pseudo") String pseudo, @RequestBody User user) {
+        try {
+            if(users.remove(getUserByPseudo(pseudo))) users.add(user);
+            return new ResponseEntity<>(users.toString(), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //Remove a user to the list
+    @RequestMapping(value = "/{pseudo}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteUser(@PathVariable("pseudo") String pseudo) {
+        try {
+            users.remove(getUserByPseudo(pseudo));
+            return new ResponseEntity<>(users.toString(), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private User getUserByPseudo(String pseudo){
+        for(User u : users){
+            if(u.getPseudo().equals(pseudo)) return u;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/org/hyperlinux/Linq/Model/User.java
@@ -0,0 +1,42 @@
+package org.hyperlinux.Linq.Model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+    @JsonProperty
+    private String pseudo;
+    @JsonProperty
+    private boolean ready;
+
+    public User() {
+    }
+
+    public User(String pseudo, boolean ready) {
+        this.pseudo = pseudo;
+        this.ready = ready;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "pseudo='" + pseudo + '\'' +
+                ", ready=" + ready +
+                '}';
+    }
+}
